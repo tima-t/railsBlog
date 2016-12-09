03d348dd916f441c885b1ad64802bcafb05e985b
tito <tmt940@abv.bg>
2016-12-09 12:56:59 +0200

    added comments

--- db/migrate/20161208162547_create_articles.rb
+++ db/migrate/20161208162547_create_articles.rb
@@ -1,10 +1,8 @@
 class CreateArticles < ActiveRecord::Migration
   def change
-	create_table :articles do |t|
-
-		t.string :title
-		t.text :body
-		t.timestamp
-	end
+    create_table :articles do |t|
+      t.string :title
+      t.text :body
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161208162547) do
+ActiveRecord::Schema.define(version: 20161209104354) do
 
   create_table "articles", force: true do |t|
-    t.string "title"
-    t.text   "body"
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
+
+  create_table "comments", force: true do |t|
+    t.string   "author_name"
+    t.text     "body"
+    t.integer  "article_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["article_id"], name: "index_comments_on_article_id"
 
 end
